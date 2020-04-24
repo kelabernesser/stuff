40ef45ed24ac3df4fa79c52288bc4e28714cfab3
Kela Bernesser <kelabernesser@gmail.com>
2020-04-24 14:41:35 -0600

    single-page-app

---
+++ Module4/react-roto-router/src/About.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function About() {
+    return (
+        <div className="aboutPage">
+            <h1 className = "pageName">
+                About
+             </h1>
+             <p className = "aboutInfo">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque iaculis ex id est rhoncus, nec elementum erat laoreet. Aliquam nec ipsum aliquam, accumsan leo ut, posuere lorem. Morbi dolor mi, mollis nec orci non, porttitor congue turpis. Integer iaculis gravida molestie. Ut et maximus quam. Integer ac commodo libero, in molestie tortor. Curabitur venenatis quam sed elit sagittis ultricies. Curabitur purus arcu, ornare sed malesuada nec, sagittis et diam.
+
+Morbi consectetur viverra orci, a imperdiet arcu sagittis eu. Nulla augue tortor, sagittis eget magna a, dignissim porttitor sapien. Nam iaculis sodales porta. Sed pharetra molestie ante in fermentum. Aenean lacinia, nisi eu suscipit rhoncus, justo ipsum varius orci, in fringilla nulla odio a odio. Nunc ut velit id nisl tincidunt dapibus. Fusce viverra quis arcu non porta. Sed vitae tellus volutpat, egestas velit ut, blandit diam. Aenean sit amet dignissim leo. Mauris metus ipsum, tincidunt et quam ultrices, posuere convallis massa. Duis posuere nisi et imperdiet fringilla. Cras ipsum erat, fringilla efficitur dui ac, finibus pellentesque enim. Ut sagittis quam a diam aliquam, et tempus ipsum facilisis.
+
+Donec finibus sed massa eget mattis. Nunc non imperdiet nisl. Curabitur congue metus et odio semper, ut vestibulum tortor molestie. Aenean eu lectus metus. Nunc ac varius felis. Vestibulum id lectus nisi. Phasellus tempus porttitor interdum. Etiam pretium auctor justo a finibus. Nunc interdum leo at odio convallis fringilla. Aenean id porttitor odio, id mattis justo. Cras metus leo, fermentum dignissim auctor ac, ultrices a eros.</p>
+
+        </div>
+
+    )
+}
+
+export default About
---
+++ Module4/react-roto-router/src/Services.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+function Services() {
+    return (
+        <div className="servicesPage">
+            <h1 className="pageName">
+                Services
+        </h1>
+            <ol>
+                <h1>Our Plumbing Services Include</h1>
+
+                <li>Drain Cleaning & Rooter Services</li>
+                <li>Water Heaters</li>
+                <li>Garbage Disposal Repair & Replace</li>
+                <li>Sump Pump Repair & Replace</li>
+                <li>French Drain Installation & Repair</li>
+
+            </ol>
+            <ol>
+                <h1>Our Electrical Services Include</h1>
+
+                <li>Electrical Inspections & Code Violations</li>
+                <li>Electrical Repair & Service</li>
+                <li>Service Upgrades</li>
+                <li>Circuit Breakers</li>
+                <li>Electrical Remodeling</li>
+
+            </ol>
+
+        </div>
+    )
+}
+
+export default Services
